ece65ca3f8a1224b7f3fa1b63621cc3cc60624d9
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-06 11:34:39 +0700

    Modal

--- app/Views/admin/transaksi.php
+++ app/Views/admin/transaksi.php
@@ -22,18 +22,8 @@
                                 <!-- <button class="au-btn-filter">
                                     <i class="zmdi zmdi-filter-list"></i>filters</button> -->
                             </div>
-                            <div class="table-data__tool-right">
-                                <button class="au-btn au-btn-icon au-btn--green au-btn--small">
-                                    <i class="zmdi zmdi-plus"></i>Browse</button>
-                                <!-- <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
-                                    <select class="js-select2" name="type">
-                                        <option selected="selected">Export</option>
-                                        <option value="">Option 1</option>
-                                        <option value="">Option 2</option>
-                                    </select>
-                                    <div class="dropDownSelect2"></div>
-                                </div> -->
-                            </div>
+                            <!-- Trigger the modal with a button -->
+                            <button type="button" class="btn btn-info btn-lg au-btn--small" data-toggle="modal" data-target="#myModal">Browse</button>
                         </div>
                         <div class="table-responsive table-responsive-data2">
                             <table class="table table-data2">
@@ -99,6 +89,29 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-lg">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
+                    <h4 class="modal-title">Pilih Barang yang ingin di transaksi</h4>
+                </div>
+                <div class="modal-body">
+                    <p>-</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" name="pilih" class="btn btn-primary btn-default" data-dismiss="modal">Pilih</button>
+                    <button type="button" name="close" class="btn btn-danger btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-12">
             <div class="copyright">
