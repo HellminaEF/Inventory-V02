4a0833b3acef92010ebf4c4609ae518104fb5b83
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-09 21:39:40 +0700

    Controller jenis : insert

--- app/Controllers/Jenis.php
+++ app/Controllers/Jenis.php
@@ -25,4 +25,25 @@ class Jenis extends BaseController
 
         return view('admin/jenis', $data);
     }
+
+    public function add_jenis()
+    {
+        $data = [
+            'title' => 'Form Tambah Jenis Barang'
+        ];
+
+        return view('insert/add_jenis', $data);
+    }
+
+    public function save_jenis()
+    {
+        $this->JenisModel->save([
+            'jenis' => $this->request->getVar('jenis'),
+            'ket' => $this->request->getVar('ket')
+        ]);
+
+        session()->setFlashdata('Pesan', 'Jenis barang berhasil ditambahkan.');
+
+        return redirect()->to('/jenis');
+    }
 }
