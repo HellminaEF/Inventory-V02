3913b2220f4e766974009831a44a440b77f1e63f
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-09 11:57:51 +0700

    Table divisi

--- app/Views/admin/divisi.php
+++ app/Views/admin/divisi.php
@@ -32,25 +32,27 @@
                                         <th>ID</th>
                                         <th>Nama Divisi</th>
                                         <th>Keterangan</th>
-                                        <th></th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>-</td>
-                                        <td>-</td>
-                                        <td>-</td>
-                                        <td>
-                                            <div class="table-data-feature">
-                                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                                    <i class="zmdi zmdi-edit"></i>
-                                                </button>
-                                                <button class="item" type="button" data-toggle="modal" data-target="#myModal" data-placement="top" title="Delete">
-                                                    <i class="zmdi zmdi-delete"></i>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    <?php foreach ($divisi as $D) : ?>
+                                        <tr>
+                                            <td scope="row">1</td>
+                                            <td><?= $D['divisi']; ?></td>
+                                            <td><?= $D['ket']; ?></td>
+                                            <td>
+                                                <div class="table-data-feature">
+                                                    <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                        <i class="zmdi zmdi-edit"></i>
+                                                    </button>
+                                                    <button class="item" type="button" data-toggle="modal" data-target="#myModal" data-placement="top" title="Delete">
+                                                        <i class="zmdi zmdi-delete"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
