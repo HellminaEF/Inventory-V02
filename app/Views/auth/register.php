3a0c49b65195eb4f73b426a4e7305cb814023a6a
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-15 11:37:13 +0700

    Register First Commit

---
+++ app/Views/auth/register.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Register | USSI Inventory</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="<?= base_url(); ?>/images/icon/8.png" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/css/main.css">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <!-- <div class="login100-pic js-tilt" data-tilt>
+                    <img src="images/icon/8.png" alt="IMG">
+                </div> -->
+
+                <form action="<?= url_to('register') ?>" method="post" class="login100-form validate-form">
+                    <?= csrf_field() ?>
+                    <span class="login100-form-title">
+                        USSI Inventory
+                    </span>
+                    
+                    <div class="wrap-input100 validate-input" data-validate="Email is required">
+                        <input class="input100 <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" type="email" name="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-at" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Username is required">
+                        <input class="input100 <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" type="text" name="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100 <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" type="password" name="pass" placeholder="<?= lang('Auth.password') ?>" autocomplete="off">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit">
+                            <?= lang('Auth.register') ?>
+                        </button>
+                    </div>
+                    <hr>
+
+                    <p class="text-center"><?= lang('Auth.alreadyRegistered') ?> <a href="<?= url_to('login') ?>"><?= lang('Auth.signIn') ?></a></p>
+
+                    <div class="text-center p-t-127">
+                        <span>Copyright &copy; PKL13 <?= date('Y'); ?></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!--===============================================================================================-->
+    <script src="<?= base_url(); ?>/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url(); ?>/vendor/bootstrap/js/popper.js"></script>
+    <script src="<?= base_url(); ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url(); ?>/vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url(); ?>/vendor/tilt/tilt.jquery.min.js"></script>
+    <script>
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
+    </script>
+    <!--===============================================================================================-->
+    <script src="<?= base_url(); ?>/js/main1.js"></script>
+
+</body>
+
+</html>
