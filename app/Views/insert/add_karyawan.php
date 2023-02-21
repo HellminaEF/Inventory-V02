718c9d8a2b45b5a31b7178332b096f4fa1026d37
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-21 13:31:31 +0700

    ubah add_karyawan

--- app/Views/insert/add_karyawan.php
+++ app/Views/insert/add_karyawan.php
@@ -10,72 +10,77 @@
         <div class="section__content section__content--p30">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="form-control">
+                    <div class="col-lg-12">
                         <div class="card">
                             <div class="card-header bg-secondary text-white">
-                                <a type="button" class="btn btn-secondary btn-sm" href="<?= base_url('karyawan') ?>"><- Back</a>
+                                <a type="button" class="btn btn-secondary btn-sm mr-3" href="<?= base_url('karyawan') ?>"><i class="fas fa-reply"></i> Back</a>
                                         Tambah <strong>Karyawan</strong>
                             </div>
                             <div class="card-body card-block">
-                                <form action="" method="post" class="form-horizontal">
+                                <form action="<?= base_url('save_karyawan') ?>" method="post" class="form-horizontal">
+                                    <?= csrf_field(); ?>
                                     <div class="row form-group">
                                         <div class="col col-md-3">
                                             <label for="id" class=" form-control-label">ID Karyawan</label>
                                         </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="id" name="id" class="form-control" required="required">
+                                        <div class="form-group col-md-7">
+                                            <input type="text" id="id" name="id" class="form-control" readonly="">
                                         </div>
                                     </div>
                                     <div class="row form-group">
                                         <div class="col col-md-3">
-                                            <label for="nama" class=" form-control-label">Nama Karyawan</label>
+                                            <label for="karyawan" class=" form-control-label">Nama Karyawan</label>
                                         </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="nama" name="nama" class="form-control" required="required">
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="divisi" class=" form-control-label">Divisi</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="divisi" name="divisi" class="form-control" required="required">
+                                        <div class="form-group col-md-7">
+                                            <input type="text" id="karyawan" name="karyawan" class="form-control" autofocus>
                                         </div>
                                     </div>
                                     <div class="row form-group">
                                         <div class="col col-md-3">
-                                            <label for="jabatan" class=" form-control-label">Jabatan</label>
+                                            <label for="id_divisi" class=" form-control-label">Divisi</label>
                                         </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="jabatan" name="jabatan" class="form-control" required="required">
+                                        <div class="form-group col-md-7">
+                                            <select name="divisi" class="form-control">
+                                                <option value="">- Pilih -</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="col col-md-3">
+                                            <label for="id_jabatan" class=" form-control-label">Jabatan</label>
+                                        </div>
+                                        <div class="form-group col-md-7">
+                                            <select name="jabatan" class="form-control">
+                                                <option value="">- Pilih -</option>
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="row form-group">
                                         <div class="col col-md-3">
                                             <label for="status" class=" form-control-label">Status</label>
                                         </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="status" name="status" class="form-control" required="required">
+                                        <div class="form-group col-md-7">
+                                            <input type="text" id="status" name="status" class="form-control">
                                         </div>
                                     </div>
                                     <div class="row form-group">
                                         <div class="col col-md-3">
                                             <label for="ket" class=" form-control-label">Keterangan</label>
                                         </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="ket" name="ket" class="form-control" required="required">
+                                        <div class="form-group col-md-7">
+                                            <input type="text" id="ket" name="ket" class="form-control">
                                         </div>
                                     </div>
-                                </form>
                             </div>
                             <div class="card-footer">
                                 <button type="submit" class="btn btn-primary btn-sm">
                                     <i class="fa fa-dot-circle-o"></i> Submit
                                 </button>
-                                <button type="reset" class="btn btn-danger btn-sm">
+                                <!-- <button type="reset" class="btn btn-danger btn-sm">
                                     <i class="fa fa-ban"></i> Reset
-                                </button>
+                                </button> -->
                             </div>
+                            </form>
                         </div>
                     </div>
                 </div>
