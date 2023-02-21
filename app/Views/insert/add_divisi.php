5a8be33f05ada6f9190a95cd082b8949ac36cc90
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-21 13:32:42 +0700

    ubah add_divisi

--- app/Views/insert/add_divisi.php
+++ app/Views/insert/add_divisi.php
@@ -8,58 +8,58 @@
     <!-- MAIN CONTENT-->
     <div class="main-content">
         <div class="section__content section__content--p30">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="form-control">
-                        <div class="card">
-                            <div class="card-header bg-secondary text-white">
-                                <a type="button" class="btn btn-secondary btn-sm" href="<?= base_url('divisi') ?>"><- Back</a>
-                                        Tambah <strong>Divisi</strong>
-                            </div>
-                            <div class="card-body card-block">
-                                <form action="<?= base_url('save_divisi') ?>" method="post" class="form-horizontal">
-                                    <?= csrf_field(); ?>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="id_divisi" class=" form-control-label">ID Divisi</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="id_divisi" name="id_divisi" class="form-control" required="required" autofocus>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="divisi" class=" form-control-label">Nama Divisi</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="divisi" name="divisi" class="form-control" required="required">
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="ket" class=" form-control-label">Keterangan</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="ket" name="ket" class="form-control" required="required">
-                                        </div>
-                                    </div>
-                            </div>
-                            <div class="card-footer">
-                                <div class="col-sm-10">
-                                    <button type="Submit" class="btn btn-primary btn-sm">
-                                        <i class="fa fa-dot-circle-o"></i> Submit
-                                    </button>
-                                </div>
-                            </div>
-                            </form>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header bg-secondary text-white">
+                            <a type="button" class="btn btn-secondary btn-sm mr-3" href="/divisi/divisi"><i class="fas fa-reply"></i> Back</a>
+                                    Tambah <strong>Divisi</strong>
                         </div>
+                        <div class="card-body card-block">
+                            <form action="/divisi/save" method="post" class="form-horizontal">
+                                <?= csrf_field(); ?>
+                                <div class="row form-group">
+                                    <div class="col col-md-3">
+                                        <label for="id_divisi" class="form-control-label">ID Divisi</label>
+                                    </div>
+                                    <div class="form-group col-md-7">
+                                        <input type="text" id="id_divisi" name="id_divisi" class="form-control" readonly="">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3">
+                                        <label for="divisi" class="form-control-label">Nama Divisi</label>
+                                    </div>
+                                    <div class="form-group col-md-7">
+                                        <input type="text" class="form-control" id="divisi" name="divisi" autofocus value="<?= old('divisi'); ?>">
+                                        <div class="invalid-feedback">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3">
+                                        <label for="ket" class=" form-control-label">Keterangan</label>
+                                    </div>
+                                    <div class="form-group col-md-7">
+                                        <input type="text" id="ket" name="ket" class="form-control" value="<?= old('divisi'); ?>">
+                                    </div>
+                                </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="col-sm-10">
+                                <button type="Submit" class="btn btn-primary btn-sm">
+                                    <i class="fa fa-dot-circle-o"></i> Submit
+                                </button>
+                            </div>
+                        </div>
+                        </form>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="copyright">
-                            <span>Copyright &copy; <?= date('Y'); ?>. All rights reserved</span>
-                        </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright">
+                        <span>Copyright &copy; PKL13 <?= date('Y'); ?>. All rights reserved</span>
                     </div>
                 </div>
             </div>
