2f585c1294196c904076a0136fe2636508c8cab5
Hellmina Enjelina Fitri <hellminae@gmail.com>
2023-02-21 13:31:10 +0700

    ubah add_jenis

--- app/Views/insert/add_jenis.php
+++ app/Views/insert/add_jenis.php
@@ -8,58 +8,56 @@
     <!-- MAIN CONTENT-->
     <div class="main-content">
         <div class="section__content section__content--p30">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="form-control">
-                        <div class="card">
-                            <div class="card-header bg-secondary text-white">
-                                <a type="button" class="btn btn-secondary btn-sm" href="<?= base_url('jenis') ?>"><- Back</a>
-                                        Tambah <strong>Data Jenis</strong>
-                            </div>
-                            <div class="card-body card-block">
-                                <form action="<?= base_url('save_jenis') ?>" method="post" class="form-horizontal">
-                                    <?= csrf_field(); ?>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="id_jenis" class="form-control-label">ID Jenis Barang</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="id_jenis" name="id_jenis" class="form-control" required="required" autofocus>
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="jenis" class="form-control-label">Jenis Barang</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="jenis" name="jenis" class="form-control" required="required">
-                                        </div>
-                                    </div>
-                                    <div class="row form-group">
-                                        <div class="col col-md-3">
-                                            <label for="ket" class="form-control-label">Keterangan</label>
-                                        </div>
-                                        <div class="form-group col-md-9">
-                                            <input type="text" id="ket" name="ket" class="form-control" required="required">
-                                        </div>
-                                    </div>
-                            </div>
-                            <div class="card-footer">
-                                <div class="col-sm-10">
-                                    <button type="submit" class="btn btn-primary btn-sm">
-                                        <i class="fa fa-dot-circle-o"></i> Submit
-                                    </button>
-                                </div>
-                            </div>
-                            </form>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header bg-secondary text-white">
+                            <a type="button" class="btn btn-secondary btn-sm mr-3" href="/jenis/jenis"><i class="fas fa-reply"></i> Back</a>
+                                    Tambah <strong>Data Jenis</strong>
                         </div>
+                        <div class="card-body card-block">
+                            <form action="/jenis/save" method="post" class="form-horizontal">
+                                <?= csrf_field(); ?>
+                                <div class="row form-group">
+                                    <div class="col col-md-3">
+                                        <label for="id_jenis" class="form-control-label">ID Jenis Barang</label>
+                                    </div>
+                                    <div class="form-group col-md-7">
+                                        <input type="text" id="id_jenis" name="id_jenis" class="form-control" readonly="">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3">
+                                        <label for="jenis" class="form-control-label">Jenis Barang</label>
+                                    </div>
+                                    <div class="form-group col-md-7">
+                                        <input type="text" id="jenis" name="jenis" class="form-control" required="required" autofocus>
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3">
+                                        <label for="ket" class="form-control-label">Keterangan</label>
+                                    </div>
+                                    <div class="form-group col-md-7">
+                                        <input type="text" id="ket" name="ket" class="form-control">
+                                    </div>
+                                </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary btn-sm">
+                                    <i class="fa fa-dot-circle-o"></i> Submit
+                                </button>
+                            </div>
+                        </div>
+                        </form>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="copyright">
-                            <span>Copyright &copy; PKL13 <?= date('Y'); ?>. All rights reserved</span>
-                        </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright">
+                        <span>Copyright &copy; PKL13 <?= date('Y'); ?>. All rights reserved</span>
                     </div>
                 </div>
             </div>
